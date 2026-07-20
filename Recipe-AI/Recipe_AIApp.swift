774a76e2b4b5842f281344362b6106fd700392aa
nikshit bhateja <iosdevelopernikshit@gmail.com>
2026-07-20 19:49:47 +0530

    Initial commit

---
+++ Recipe-AI/Recipe_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AIApp.swift
+//  Recipe-AI
+//
+//  Created by Nikk Bhateja on 20/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
